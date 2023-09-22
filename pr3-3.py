ac63577dc38869f359a53777384a498595c7b763
Zeroxlovv <145494537+Zeroxlovv@users.noreply.github.com>
2023-09-22 20:05:54 +0300

    Add files via upload

---
+++ pr3-3.py
@@ -0,0 +1,10 @@
+a=input('Введите число ')
+b=[]
+c=[]
+for i in a:
+    if int(i)%2==0:
+        b.append(i)
+    else:
+        c.append(i)
+print(b,' - чётные числа')
+print(c,' - нечётные числа')
